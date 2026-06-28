5021ea140b1f16900b7bce49ad89c62c8dde5bbd
Aditya Argadinata <93075183+AdityaArgadinata@users.noreply.github.com>
2026-06-28 20:19:38 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kixup.online',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [mdx(), sitemap()],
 
   vite: {
